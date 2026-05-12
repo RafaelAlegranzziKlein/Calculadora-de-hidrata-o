5298162e0c28cc479e9c92a0b417c543ba791019
Rafael Alegranzzi Klein <rafaelklein2007@gmail.com>
2026-05-12 19:19:00 -0300

    correção de codigo e melhoria no mesmo

--- app/src/main/java/br/ulbra/calculadoradehidratao/MainActivity.java
+++ app/src/main/java/br/ulbra/calculadoradehidratao/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
     RadioButton rbSedentario, rbModerado, rbIntenso;
 
+    RadioGroup rgGrupo1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,95 +39,66 @@ public class MainActivity extends AppCompatActivity {
         rbSedentario = findViewById(R.id.rbtSedentario);
         rbModerado = findViewById(R.id.rbtModerado);
         rbIntenso = findViewById(R.id.rbtIntenso);
+        rgGrupo1 = findViewById(R.id.rgBotao);
         btCalcular = findViewById(R.id.btnCalcular);
         btCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                double peso, consumoBase, consumoTotal, consumoRes;
-
-                int aguaConsumida, sedentario, moderado, inteso;
-
-                sedentario = 0;
-                moderado = 300;
-                inteso = 600;
-
-
-                if (edPeso.getText().toString().isEmpty() || edIdade.getText().toString().isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Os capos de peso e idade devem conter algo", Toast.LENGTH_LONG).show();
-                } else if (Double.parseDouble(edPeso.getText().toString()) <= 0) {
-                    Toast.makeText(MainActivity.this, "Tu é um papel ? 🤨", Toast.LENGTH_SHORT).show();
-                } else if (!(Integer.parseInt(edIdade.getText().toString()) > 0)) {
-                    Toast.makeText(MainActivity.this, "Tabom ser de outro plano que consegue mexer no celular sem um cerebro minimamente desenvolvido", Toast.LENGTH_SHORT).show();
-                } else if (rbSedentario.isSelected()) {
-
-                    aguaConsumida = Integer.parseInt(edIdade.getText().toString());
-                    peso = Double.parseDouble(edPeso.getText().toString());
-
-                    String res1;
-
-                    consumoBase = peso * 35;
-                    consumoTotal = consumoBase + sedentario;
-                    consumoRes = consumoTotal - aguaConsumida;
-
-                    DecimalFormat df = new DecimalFormat("0.00");
-
-                    res1 = "\nIdade:" + edIdade.getText() +
-                            "\nMeta Diária:" + df.format(consumoTotal) + " ml" +
-                            "\nCusto Estimado: R$" + df.format(consumoRes);
-
-                    AlertDialog.Builder dialogo = new AlertDialog.Builder(MainActivity.this);
-                    dialogo.setTitle("Resultado");
-                    dialogo.setMessage(res1);
-                    dialogo.setNeutralButton("OK", null);
-                    dialogo.show();
-
-                } else if (rbModerado.isSelected()) {
-
-
-                    aguaConsumida = Integer.parseInt(edIdade.getText().toString());
-                    peso = Double.parseDouble(edPeso.getText().toString());
-
-                    String res1;
-
-                    consumoBase = peso * 35;
-                    consumoTotal = consumoBase + moderado;
-                    consumoRes = consumoTotal - aguaConsumida;
-
-                    DecimalFormat df = new DecimalFormat("0.00");
-
-                    res1 = "\nIdade:" + edIdade.getText() +
-                            "\nMeta Diária:" + df.format(consumoTotal) + " ml" +
-                            "\nCusto Estimado: R$" + df.format(consumoRes);
-
-                    AlertDialog.Builder dialogo1 = new AlertDialog.Builder(MainActivity.this);
-                    dialogo1.setTitle("Resultado");
-                    dialogo1.setMessage(res1);
-                    dialogo1.setNeutralButton("OK", null);
-                    dialogo1.show();
-                } else {
-
-
-                    aguaConsumida = Integer.parseInt(edIdade.getText().toString());
-                    peso = Double.parseDouble(edPeso.getText().toString());
-
-                    String res1;
-
-                    consumoBase = peso * 35;
-                    consumoTotal = consumoBase + inteso;
-                    consumoRes = consumoTotal - aguaConsumida;
-
-                    DecimalFormat df = new DecimalFormat("0.00");
-
-                    res1 = "\nIdade:" + edIdade.getText() +
-                            "\nMeta Diária:" + df.format(consumoTotal) + " ml" +
-                            "\nCusto Estimado: R$" + df.format(consumoRes);
-
-                    AlertDialog.Builder dialogo2 = new AlertDialog.Builder(MainActivity.this);
-                    dialogo2.setTitle("Resultado");
-                    dialogo2.setMessage(res1);
-                    dialogo2.setNeutralButton("OK", null);
-                    dialogo2.show();
+                // 1. Verificação de campos vazios (incluindo a água consumida)
+                if (edPeso.getText().toString().isEmpty() ||
+                        edIdade.getText().toString().isEmpty() ||
+                        edAguaConsumida.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Preencha todos os campos!", Toast.LENGTH_LONG).show();
+                    return;
                 }
+
+                // 2. Conversão de valores
+                double peso = Double.parseDouble(edPeso.getText().toString());
+                int idade = Integer.parseInt(edIdade.getText().toString());
+                double jaConsumido = Double.parseDouble(edAguaConsumida.getText().toString());
+
+                // 3. Validações de sanidade
+                if (peso <= 0) {
+                    Toast.makeText(MainActivity.this, "Tu é um papel? 🤨", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (idade <= 0) {
+                    Toast.makeText(MainActivity.this, "Idade inválida!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // 4. Lógica de Adicional por Atividade
+                int adicionalAtividade = 0;
+                if (rbSedentario.isChecked()) {
+                    adicionalAtividade = 0;
+                } else if (rbModerado.isChecked()) {
+                    adicionalAtividade = 300;
+                } else if (rbIntenso.isChecked()) {
+                    adicionalAtividade = 600;
+                } else {
+                    Toast.makeText(MainActivity.this, "Selecione o nível de atividade!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // 5. Cálculos (Meta base de 35ml por kg)
+                double consumoTotal = (peso * 35) + adicionalAtividade;
+                double faltaBeber = consumoTotal - jaConsumido;
+
+                // Se o resultado for negativo, ele já bateu a meta
+                String msgFalta = faltaBeber <= 0 ? "Meta atingida!" : "Faltam: " + new DecimalFormat("0").format(faltaBeber) + " ml";
+
+                // 6. Exibição Única
+                DecimalFormat df = new DecimalFormat("0.00");
+                String resultadoFinal = "Idade: " + idade +
+                        "\nMeta Diária: " + df.format(consumoTotal) + " ml" +
+                        "\nJá consumido: " + df.format(jaConsumido) + " ml" +
+                        "\n" + msgFalta;
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Resultado da Hidratação")
+                        .setMessage(resultadoFinal)
+                        .setNeutralButton("OK", null)
+                        .show();
             }
         });
     }
